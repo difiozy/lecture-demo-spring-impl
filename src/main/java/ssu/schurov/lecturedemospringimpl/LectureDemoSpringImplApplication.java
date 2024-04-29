f4e2a338591c10a0a37620bbd0c7116d4892f034
schurovdi <shurow7@gmail.com>
2024-04-29 22:50:40 +0400

    Initial commit

---
+++ src/main/java/ssu/schurov/lecturedemospringimpl/LectureDemoSpringImplApplication.java
@@ -0,0 +1,13 @@
+package ssu.schurov.lecturedemospringimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureDemoSpringImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureDemoSpringImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssu/schurov/lecturedemospringimpl/LectureDemoSpringImplApplicationTests.java
@@ -0,0 +1,13 @@
+package ssu.schurov.lecturedemospringimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureDemoSpringImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
